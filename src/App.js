8c7bdef5fb7e2277da08f7a64f42ada2b20b8c78
HarshitDave30 <hdave04@gmail.com>
2023-06-10 07:26:20 +0100

    Login With Object

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./style.css";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import Login from "./pages/Login";
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route index element={<Home />} />
-          <Route path="cart" element={<Cart />} />
-          <Route path="login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Navigate to="/login" replace />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,17 +1,22 @@
-import React from 'react'
-import TextField from '@mui/material/TextField';
-import { content } from './Content';
+import React from "react";
+import TextField from "@mui/material/TextField";
+import { content } from "./Content";
 
 export default function Search({ setProduct }) {
+  const handleChange = (e) => {
+    const searchFilter = content.filter((element, index) =>
+      element.type.toLowerCase().includes(e.toLowerCase())
+    );
+    setProduct(searchFilter);
+  };
 
-    const handleChange = (e) => {
-        const searchFilter = content.filter((element, index) =>
-            element.type.toLowerCase().includes(e.toLowerCase())
-        );
-        setProduct(searchFilter)
-    }
-
-    return (
-        <TextField id="standard-basic" onChange={(e) => handleChange(e.target.value)} label="Search..." sx={{ width: '100%', marginBottom: 5 }} variant="standard" />
-    )
+  return (
+    <TextField
+      id="standard-basic"
+      onChange={(e) => handleChange(e.target.value)}
+      label="Search..."
+      sx={{ width: "100%", marginBottom: 5 }}
+      variant="standard"
+    />
+  );
 }
